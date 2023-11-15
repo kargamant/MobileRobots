c824585d79e5b8aeb54b224390dc3866bc1449e4
kargamant <firexholms@gmail.com>
2023-11-15 10:41:39 +0300

    rmd build folder

--- Game/MyGame/build/CMakeFiles/SYNC_RESOURCE-MyGame.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SYNC_RESOURCE-MyGame"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SYNC_RESOURCE-MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
