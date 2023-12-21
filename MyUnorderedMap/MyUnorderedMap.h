48df9d7f464065d2a684ad34941d98ea86e8b18d
kargamant <firexholms@gmail.com>
2023-12-21 13:45:49 +0300

    Made implimentation for constructors

--- MyUnorderedMap/MyUnorderedMap.h
+++ MyUnorderedMap/MyUnorderedMap.h
@@ -23,6 +23,19 @@ struct Bucket
 
 	Bucket() : end(new Item<V>) { end->isEnd = true; first = end; last = end; }
 
+	~Bucket()
+	{
+		Item<V>* ptr = first;
+		while (!ptr->isEnd)
+		{
+			Item<V>* next = ptr->next;
+			delete ptr;
+			ptr = next;
+		}
+		delete ptr;
+		size = 0;
+	}
+
 	bool isEmpty() { return first == end; }
 
 	std::pair<Item<V>*, bool> push(V value)
@@ -212,7 +225,7 @@ static_assert(std::forward_iterator<UnorderedMapIterator<int, false>>);
 template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
 class MyUnorderedMap
 {
-	
+public:	
 	typedef Key key_type;
 	typedef T mapped_type;
 	typedef std::pair<const Key, T> value_type;
@@ -233,61 +246,12 @@ class MyUnorderedMap
 	typedef Bucket<value_type> bucket_type;
 	typedef std::pair<iterator, bool> insert_return_type;
 
-	static const size_type DEFAULT_BUCKET_COUNT = 7;
-	size_type buckets_count=0;
-	size_type max_buckets_count=DEFAULT_BUCKET_COUNT;
-	bucket_type* buckets=nullptr;
-	node_type* before_begin=nullptr;
-	node_type* past_the_last=nullptr;
-	float max_load_factor = 1;
-	float load_factor = 0;
-
-	void alloc_bucket_array(size_type size)
-	{
-		buckets = new bucket_type[size];
-		buckets_count = 0;
-		max_buckets_count = size;
-	}
-
-	void release_bucket_array()
-	{
-		delete[] buckets;
-		buckets_count = 0;
-		max_buckets_count = 0;
-	}
-
-	iterator find_item(key_type key)
-	{
-		size_type position = Hash(key);
-		iterator itr = iterator(buckets[position].first);
-		while (itr.it->value.first != key && !itr.it->isEnd) ++itr;
-		return itr;
-	}
-
-	insert_return_type insert_item(value_type nitem)
-	{
-		size_type position = Hash(nitem.first);
-		auto result=buckets[position].push(nitem);
-		if (result.first == buckets[position].first) buckets_count++;
-		return result;
-	}
-
-	iterator erase_item(key_type key)
-	{
-		size_type position = Hash(key);
-		iterator itr = iterator(buckets[position].first);
-		while (itr.it->next->value.first != key && !itr.it->next->isEnd) ++itr;
-		buckets[position].erase(itr.it);
-		if (buckets[position].size == 0) buckets_count--;
-		return itr;
-	}
 	
 	//MyUnorderedMap();
 	// : bucket_size(DEFAULT_BUCKET_SIZE), buckets(new bucket_type[DEFAULT_BUCKET_SIZE]),
 	MyUnorderedMap(size_type bucket_count=DEFAULT_BUCKET_COUNT, const hasher& hash = Hash(), const key_equal& equal = KeyEqual());
 
-	template<class Iter>
-	MyUnorderedMap(Iter first, Iter last, size_type bucket_count=DEFAULT_BUCKET_COUNT, const hasher& hash=Hash(), const key_equal& equal=KeyEqual());
+	MyUnorderedMap(iterator first, iterator last, size_type bucket_count=DEFAULT_BUCKET_COUNT, const hasher& hash=Hash(), const key_equal& equal=KeyEqual());
 
 	MyUnorderedMap(const MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& um);
 
@@ -300,21 +264,24 @@ class MyUnorderedMap
 	MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& operator=(const MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& um2);
 	MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& operator=(MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>&& um2);
 	MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& operator=(std::initializer_list<value_type> il);
-	hasher hash_function() { return Hash; }
-	key_equal key_eq() { return KeyEqual; }
+	hasher hash_function() { return hasher(); }
+	key_equal key_eq() { return key_equal(); }
 
 	template<class... Args>
 	insert_return_type emplace(Args&&... args);
 	template<class... Args>
 	iterator emplace_hint(const_iterator p, Args&&... args);
+	template<class... Args>
+	insert_return_type try_emplace(const key_type& key, Args&&... args);
 	insert_return_type insert(const value_type& value);
 	insert_return_type insert(value_type&& value);
 	void insert(iterator first, iterator last);
 	void insert(std::initializer_list<value_type> il); //insert(il.begin(), il.end());
 	insert_return_type insert(node_type&& nh);
+	insert_return_type insert_or_assign(const key_type& key, mapped_type&& obj);
 	node_type* extract(key_type& key);
 	node_type* extract(const_iterator itr);
-	template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+	//template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
 	void merge(MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& other);
 	size_type erase(key_type& key);
 	iterator erase(iterator itr);
@@ -323,8 +290,8 @@ class MyUnorderedMap
 	iterator find(key_type& key);
 	const_iterator find(key_type& key) const;
 	bool contains(key_type& key); //find(key)!=end()
-	size_type bucket_count() { return buckets_count; }
-	size_type max_bucket_count() { return max_buckets_count; }
+	size_type bucket_count() { return bc; }
+	size_type max_bucket_count() { return mbc; }
 	size_type bucket(key_type& key) { return Hash(key); }
 	size_type bucket_size(size_type n) { return buckets[n].size; }
 
@@ -334,9 +301,9 @@ class MyUnorderedMap
 	const_iterator end(size_type n) const { return iterator(buckets[n].end); }
 	const_iterator cbegin(size_type n) const { return iterator(buckets[n].first); }
 	const_iterator cend(size_type n) const { return iterator(buckets[n].end); }
-	float load_factor() { return load_factor; }
-	float max_load_factor() { return max_load_factor; }
-	void max_load_factor(float nmlf) { max_load_factor = nmlf; }
+	float load_factor() { return lf; }
+	float max_load_factor() { return max_lf; }
+	void max_load_factor(float nmlf) { max_lf = nmlf; }
 
 	allocator_type get_allocator() const noexcept;
 	
@@ -350,6 +317,129 @@ class MyUnorderedMap
 	size_type max_size() const noexcept;
 	size_type size() const noexcept;
 
+	//template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+	void swap(MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& other);
 	
+	mapped_type& at(const key_type& key);
+	mapped_type& operator[](const key_type& key);
+	mapped_type& operator[](key_type&& key);
+	const mapped_type& at(const key_type& key) const;
+private:
+	static const size_type DEFAULT_BUCKET_COUNT = 73;
+	const hasher hash=Hash();
+	const key_equal equal = KeyEqual();
+	size_type bc=0;
+	size_type mbc=DEFAULT_BUCKET_COUNT;
+	bucket_type* buckets=nullptr;
+	node_type* before_begin=nullptr;
+	node_type* past_the_last=nullptr;
+	float max_lf = 1;
+	float lf = 0;
 
+	void alloc_bucket_array(size_type size)
+	{
+		buckets = new bucket_type[size];
+		bc = 0;
+		mbc = size;
+	}
+
+	void release_bucket_array()
+	{
+		delete[] buckets;
+		bc = 0;
+		mbc = 0;
+	}
+
+	iterator find_item(key_type key)
+	{
+		size_type position = hash(key);
+		iterator itr = iterator(buckets[position].first);
+		while (!equal(itr.it->value.first, key) && !itr.it->isEnd) ++itr;
+		return itr;
+	}
+
+	insert_return_type insert_item(value_type nitem)
+	{
+		size_type position = hash(nitem.first);
+		auto result=buckets[position].push(nitem);
+		if (result.first == buckets[position].first) bc++;
+		return result;
+	}
+
+	iterator erase_item(key_type key)
+	{
+		size_type position = Hash(key);
+		iterator itr = iterator(buckets[position].first);
+		while (!equal(itr.it->next->value.first, key) && !itr.it->next->isEnd) ++itr;
+		buckets[position].erase(itr.it);
+		if (buckets[position].size == 0) bc--;
+		return itr;
+	}
 };
+
+
+template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>::MyUnorderedMap(size_type bucket_count=DEFAULT_BUCKET_COUNT, const hasher& hash = Hash(), const key_equal& equal = KeyEqual()) : hash(hash), equal(equal)
+{
+	before_begin = new node_type;
+	past_the_last = new node_type;
+	alloc_bucket_array(bucket_count);
+}
+
+
+template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>::MyUnorderedMap(UnorderedMapIterator<std::pair<const Key, T>, false> first, UnorderedMapIterator<std::pair<const Key, T>, false> last, size_type bucket_count=DEFAULT_BUCKET_COUNT, const hasher& hash=Hash(), const key_equal& equal=KeyEqual()) : hash(hash), equal(equal)
+{
+	before_begin = new node_type;
+	past_the_last = new node_type;
+	alloc_bucket_array(bucket_count);
+	insert(first, last);
+}
+
+
+template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>::MyUnorderedMap(const MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>& um) : hash(um.hash), equal(um.equal), bc(um.bc), mbc(um.mbc) 
+{
+	alloc_bucket_array(mbc);
+	before_begin=new node_type;
+	past_the_last=new node_type;
+	insert(um.begin(), um.end());
+}
+
+
+template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>::MyUnorderedMap(MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>&& um) : hash(um.hash), equal(um.equal), bc(um.bc), mbc(um.mbc)
+{
+	before_begin = new node_type;
+	past_the_last = new node_type;
+	alloc_bucket_array(mbc);
+	insert(um.begin(), um.end());
+	um.release_bucket_array();
+	delete um.before_begin;
+	delete um.past_the_last;
+}
+
+template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>::MyUnorderedMap(std::initializer_list<value_type> il, size_type bucket_count = DEFAULT_BUCKET_COUNT, const hasher& hash = Hash(), const key_equal& equal = KeyEqual()) : hash(um.hash), equal(um.equal)
+{
+	before_begin = new node_type;
+	past_the_last = new node_type;
+	alloc_bucket_array(bucket_count);
+	for(auto itr: il)
+	{
+		insert(*itr);
+	}
+}
+
+template<std::default_initializable Key, std::default_initializable T, class Hash, class KeyEqual, class Allocator>
+MyUnorderedMap<Key, T, Hash, KeyEqual, Allocator>::~MyUnorderedMap()
+{
+	release_bucket_array();
+	delete before_begin;
+	delete past_the_last;
+}
+
+
+
+
+
