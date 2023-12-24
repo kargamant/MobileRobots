3a849861d3198f9238e3ba81142c2fb352c3fb2d
kargamant <firexholms@gmail.com>
2023-12-24 18:13:44 +0300

    Made coverage script that automatically collects gcda and gcno files and generates report

--- testing/cover.cpp
+++ testing/cover.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <format>
 
 int main()
 {
-	std::system("lcov -t \"test-testing\" -o test-testing.info -c -d .");
+	std::vector<std::string> build_dirs = {"Field", "Interfaces", "Modules", "Platforms", "testing"};
+	for (auto dir : build_dirs)
+	{
+		std::string gcno_command = std::format("find ../build/{}/CMakeFiles/{}.dir -name \"*.gcno\" -exec ", dir, dir);
+		std::string gcda_command = std::format("find ../build/{}/CMakeFiles/{}.dir -name \"*.gcda\" -exec ", dir, dir);
+		gcno_command += "cp {} gc_info \\;";
+		gcda_command += "cp {} gc_info \\;";
+		std::cout << gcno_command << std::endl;
+		std::system(gcno_command.c_str());
+		std::system(gcda_command.c_str());
+	}
+	std::system("lcov -t \"test-testing\" -o test-testing.info -c -d gc_info");
 	std::system("genhtml -o report test-testing.info");	
 	return 0;
 }
